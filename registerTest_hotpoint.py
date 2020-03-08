913bd93f07450dd258c962db234600789513a392
362732552@qq.com <362732552@qq.com>
2020-03-08 22:02:18 +0800

    modify test case

--- registerTest_hotpoint.py
+++ registerTest_hotpoint.py
@@ -1,7 +1,7 @@
 #encoding=utf-8
 import requests,json,sys
 #address=r'http://172.20.10.4:8080/front/register'
-address = "http://"+sys.argv[1]+'/front/register'
+address = "http://"+sys.argv[1]+'/interface_proj/front/register'
 '''用户注册'''
 def register():
     global pwd
